2daa7043b39c2624ad0e45af9e261145f74eb2f3
AlexMeadows <alex0meadows@gmail.com>
2015-11-05 22:08:06 -0600

    changed the middleware so functions can be hit from javascript

--- lib/middleware.rb
+++ lib/middleware.rb
@@ -6,8 +6,7 @@ require 'uri'
 require 'date'
 
 # Start Database Middleware
-def dbAddBook(title, subtitle, author, isbn, edition, publication_year, user_id, location)
-
+def mwAddBook(title, subtitle, author, isbn, edition, publication_year, user_id, location)
   uri = URI.parse("http://localhost:#{settings.port}/api/db/add-book")
   uri.query = URI.encode_www_form(
     'title' => title,
@@ -23,65 +22,69 @@ def dbAddBook(title, subtitle, author, isbn, edition, publication_year, user_id,
   return content
 end
 
-def dbRemoveBook(bookId)
+def mwRemoveBook(book_id)
   content = open("http://localhost:#{settings.port}/api/db/remove-book?book_id=#{bookId}").read
   return content
 end
 
-def dbGetBook(bookId)
-  content = open("http://localhost:#{settings.port}/api/db/get-book?book_id=#{bookId}").read
+get '/api/mw/get-book' do
+  book_id = params[:book_id]  
+  content = open("http://localhost:#{settings.port}/api/db/get-book?book_id=#{book_id}").read
   return content
 end
 
-def dbGetBooks()
+get '/api/mw/get-books' do
   content = open("http://localhost:#{settings.port}/api/db/get-books").read
   return content
 end
 
-def dbGetUserBooks(userId)
-  content = open("http://localhost:#{settings.port}/api/db/get-user-books?user_id=#{userId}").read
+get '/api/mw/get-user-books' do
+  user_id = params[:user_id]
+  content = open("http://localhost:#{settings.port}/api/db/get-user-books?user_id=#{user_id}").read
   return content
 end
 
-def dbSearchBooks(searchField, searchBy)
+get '/api/mw/search-books' do
+  search_field = params[:search_field]
+  search_by = params[:search_by]
 
-  return nil unless ['title', 'author', 'isbn', 'edition', 'publication_year', 'location'].include? searchField
+  return nil unless ['title', 'author', 'isbn', 'edition', 'publication_year', 'location'].include? search_field
 
-  if searchField == 'title'
-    content = open("http://localhost:#{settings.port}/api/db/search-books-by-title?search_by=#{searchBy}").read
+  if search_field == 'title'
+    content = open("http://localhost:#{settings.port}/api/db/search-books-by-title?search_by=#{search_by}").read
   else
-    content = open("http://localhost:#{settings.port}/api/db/search-books?search_field=#{searchField}&search_by=#{searchBy}").read
+    content = open("http://localhost:#{settings.port}/api/db/search-books?search_field=#{search_field}&search_by=#{search_by}").read
   end 
-
   return content
 end
 
 ## Checkout Related
-def dbGetCheckout(checkoutId)
-  content = open("http://localhost:#{settings.port}/api/db/get-checkout?checkout_id=#{checkoutId}").read
+get '/api/mw/get-checkout' do
+  checkout_id = params[:checkout_id]
+  content = open("http://localhost:#{settings.port}/api/db/get-checkout?checkout_id=#{checkout_id}").read
   return content
 end
 
-def dbGetUserCheckouts(userId)
-  content = open("http://localhost:#{settings.port}/api/db/get-user-checkouts?user_id=#{userId}").read
+get '/api/mw/get-user-checkouts' do
+  user_id = params[:user_id]
+  content = open("http://localhost:#{settings.port}/api/db/get-user-checkouts?user_id=#{user_id}").read
   return content
 end
 
-def dbGetCheckouts()
+get '/api/mw/get-checkouts' do
   content = open("http://localhost:#{settings.port}/api/db/get-checkouts").read
   return content
 end
 
-def dbCheckoutBook(bookId, userId)
+def mwCheckoutBook(book_id, user_id)
   current_time = DateTime.now
   # Add two weeks to the current date
   due_time = Time.now + (2*7*24*60*60)
   
-
   # Get the dates from the vareables
-  checkoutDate = current_time.strftime "%Y-%m-%d"
-  dueDate = due_time.strftime "%Y-%m-%d"
-
+  checkout_date = current_time.strftime "%Y-%m-%d"
+  due_date = due_time.strftime "%Y-%m-%d"
+  
   uri = URI.parse("http://localhost:#{settings.port}/api/db/checkout-book")
   uri.query = URI.encode_www_form(
     'book_id' => bookId,
@@ -93,11 +96,10 @@ def dbCheckoutBook(bookId, userId)
   return content
 end
 
-def dbReturnBook(checkoutId, returnCondition)  
+def mwReturnBook(checkout_id, return_condition) 
   current_time = DateTime.now
-
-  returnDate = current_time.strftime "%Y-%m-%d"
-
+  return_date = current_time.strftime "%Y-%m-%d"
+  
   uri = URI.parse("http://localhost:#{settings.port}/api/db/return-book")
   uri.query = URI.encode_www_form(
     'checkout_id' => checkoutId,
