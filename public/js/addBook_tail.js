f5da56fa78ab4a18b5d86fc1227ba9b757c262e7
Isacc Davis <IsaccDavis@letu.edu>
2015-11-19 00:15:11 -0600

    Improved lightbox styling, integraded ISBN detection, initial feedback implementation

--- public/js/addBook_tail.js
+++ public/js/addBook_tail.js
@@ -23,12 +23,35 @@ window.onload = function() {
 
         Quagga.start();
         var previousCode = "";
+
+        Quagga.onProcessed(function(result) {
+          var drawingCtx = Quagga.canvas.ctx.overlay,
+              drawingCanvas = Quagga.canvas.dom.overlay;
+
+          if (result) {
+            if (result.boxes) {
+              drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+              result.boxes.filter(function (box) {
+                return box !== result.box;
+              }).forEach(function (box) {
+                Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+              });
+            }
+
+            if (result.box) {
+              Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+            }
+
+            if (result.codeResult && result.codeResult.code) {
+              Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+            }
+          }
+        });
+
         Quagga.onDetected(function(data) {
-                            if(data.codeResult.code != previousCode) {
-                                previousCode = data.codeResult.code;
-                                alert(previousCode);
-                            }
-                        });
+          var input = data.codeResult.code;
+          document.getElementById('isbnInput').value = input;
+          insertTemplateByPath('add_book_form', 'add_book_form', '/api/google-api/isbn-info?isbn='+input);
       }
     );
   });
