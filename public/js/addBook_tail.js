77a9e30251c996620585b66a69dd71145f86e8b8
Isacc Davis <IsaccDavis@letu.edu>
2015-11-18 20:37:10 -0600

    Inserted Quagga viewport, improved lightbox styling

--- public/js/addBook_tail.js
+++ public/js/addBook_tail.js
@@ -6,5 +6,30 @@ window.onload = function() {
 
   document.getElementById('scanButton').addEventListener('click', function() {
     document.getElementById('scan_barcode_lightbox').style.visibility = "visible";
+
+    Quagga.init(
+      {
+        inputStream: {
+            constraints: {
+                width: '100%',
+                height: '100%'
+            }
+        },
+        decoder: {
+            drawBoundingBox: true,
+            drawScanLine: true,
+            readers: ['ean_reader']
+        }
+      },
+      function(error) {
+        if(error) {
+            console.log(error);
+            return;
+        }
+        console.log("Starting Quagga...");
+
+        Quagga.start();
+      }
+    );
   });
 };
